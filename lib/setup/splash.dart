a676d42c2dbfec6a18c7120674ebfd371d6ea9d8
ibrahimbtaz <ibrahimbtaz@gmail.com>
2022-10-24 14:25:47 +0700

    feat : [SI] Menambahkan Splash Screen dan Assets

---
+++ lib/setup/splash.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class splashscreen extends StatefulWidget {
+  const splashscreen({Key? key}) : super(key: key);
+
+  @override
+  State<splashscreen> createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (context) {
+          return splashscreen();
+        },
+      ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          color: Colors.white,
+          child: Center(
+            child: Image(
+              width: 240,
+              height: 240,
+              image: AssetImage("lib/asset/logo.png"),
+              // repeat: ImageRepeat.repeat,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
