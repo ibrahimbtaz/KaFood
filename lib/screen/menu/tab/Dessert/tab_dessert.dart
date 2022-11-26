d698f869f2ceddd62edf28a26f6a594ab64a13ed
ibrahimbtaz <ibrahimbtaz@gmail.com>
2022-11-26 19:02:28 +0700

    feat : [SI] add menu

--- lib/screen/menu/tab/Dessert/tab_dessert.dart
+++ lib/screen/menu/tab/Dessert/tab_dessert.dart
@@ -3,28 +3,33 @@ import 'package:mycatering/screen/menu/tab/Dessert/title_dessert.dart';
 
 class DessertTab extends StatelessWidget {
   List DessertOnSale = [
-    // ["Cake Chocolate", "36", Colors.blueGrey, "assets/images/cake-1.png"],
-    // ["Cake Chocolate", "45", Colors.blueGrey, "assets/images/cake-1.png"],
-    // ["Cake Chocolate", "84", Colors.blueGrey, "assets/images/cake-1.png"],
-    // ["Cake Chocolate", "95", Colors.blueGrey, "assets/images/cake-1.png"],
+    ["Cake Chocolate", "36", "cake-1.png"],
+    ["Cake Chocolate", "45", "cake-1.png"],
+    ["Cake Chocolate", "84", "cake-1.png"],
+    ["Cake Chocolate", "95", "cake-1.png"],
   ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.symmetric(
+        vertical: 25,
+        horizontal: 5,
+      ),
       child: GridView.builder(
         itemCount: DessertOnSale.length,
         padding: const EdgeInsets.all(12),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          childAspectRatio: 1 / 1.5,
+          childAspectRatio: 0.8,
+          crossAxisSpacing: 25,
+          mainAxisSpacing: 20,
         ),
         itemBuilder: (context, index) {
           return DessertTitle(
             DessertVariant: DessertOnSale[index][0],
             DessertPrice: DessertOnSale[index][1],
-            DessertColor: DessertOnSale[index][2],
-            imageDessertName: DessertOnSale[index][3],
+            imageDessertName: DessertOnSale[index][2],
           );
         },
       ),
