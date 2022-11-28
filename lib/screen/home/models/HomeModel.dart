ad93f537875a45ce437c8d0dbdae7026a9e3782c
ibrahimbtaz <ibrahimbtaz@gmail.com>
2022-11-28 12:53:46 +0700

    feat : [SI] add model menu

--- lib/screen/home/models/HomeModel.dart
+++ lib/screen/home/models/HomeModel.dart
@@ -68,3 +68,79 @@ List<FoodModel> foodList = [
     rate: 4.8,
   ),
 ];
+
+
+List<FoodModel> dessertList = [
+  FoodModel(
+    id: 1,
+    price: 3.6,
+    name: "Klepon",
+    image: "dessertklepon-10.png",
+    description:
+        "Tumpeng is an Indonesian cone-shaped rice dish with side dishes of vegetables and meat originating from Javanese cuisine of Indonesia. Traditionally featured in the slamatan ceremony, the rice is made by using a cone-shaped woven bamboo container. The rice itself may be plain steamed rice, uduk rice, or yellow rice.",
+    rate: 4.2,
+  ),
+  FoodModel(
+    id: 2,
+    price: 4.5,
+    name: "Martabak Manis",
+    image: "juice.jpg",
+    description:
+        "Juice is a drink made from the extraction or pressing of the natural liquid contained in fruit and vegetables. It can also refer to liquids that are flavored with concentrate or other biological food sources, such as meat or seafood, such as clam juice.",
+    rate: 4.0,
+  ),
+  FoodModel(
+    id: 3,
+    price: 4.4,
+    name: "Kue Lapis",
+    image: "dessertkuelapis-9.png",
+    description:
+        "Soft Red Velvet cake layered with thick sweet cream and grated cheese. The sprinkling part uses the remaining grated or coarsely chopped cake so that no material is wasted. Come on, try the recipe for this weekend.",
+    rate: 4.7,
+  ),
+  FoodModel(
+    id: 4,
+    price: 4.3,
+    name: "Japanese Cheese",
+    image: "dessertjapanesecheesecake-3.png",
+    description:
+        "Snack Box is a complement to consumption that is usually present at an event, for meetings, gatherings, social gatherings, house celebrations, building inauguration up to recitation.",
+    rate: 4.5,
+  ),
+  FoodModel(
+    id: 5,
+    price: 4.7,
+    name: "Kue Pukis",
+    image: "dessertkuepukis-8.png",
+    description:
+        "Chocolate cake is a cake flavored with melted chocolate, cocoa powder, or both. This cake is perfect for Birthday Events",
+    rate: 5.0,
+  ),
+  FoodModel(
+    id: 6,
+    price: 5.2,
+    name: "Red Velvet",
+    image: "dessertredvelvetstuffednutella-4.png",
+    description:
+        "Fruit salad is a dish consisting of various kinds of fruit, sometimes served in a liquid, either their juices or a syrup. In different forms, fruit salad can be served as an appetizer, a side salad. When served as an appetizer, a fruit salad is sometimes known as a fruit cocktail, or fruit cup.",
+    rate: 4.8,
+  ),
+  FoodModel(
+    id: 7,
+    price: 5.5,
+    name: "Nutella Brownis",
+    image: "dessertnutellafudgybrownis-1.png",
+    description:
+        "Fruit salad is a dish consisting of various kinds of fruit, sometimes served in a liquid, either their juices or a syrup. In different forms, fruit salad can be served as an appetizer, a side salad. When served as an appetizer, a fruit salad is sometimes known as a fruit cocktail, or fruit cup.",
+    rate: 4.8,
+  ),
+  FoodModel(
+    id: 8,
+    price: 4.9,
+    name: "Nutella Lamington",
+    image: "dessertnutellalamington-5.png",
+    description:
+        "Fruit salad is a dish consisting of various kinds of fruit, sometimes served in a liquid, either their juices or a syrup. In different forms, fruit salad can be served as an appetizer, a side salad. When served as an appetizer, a fruit salad is sometimes known as a fruit cocktail, or fruit cup.",
+    rate: 4.8,
+  ),
+];
--- lib/screen/menu/tab/Dessert/tab_dessert.dart
+++ lib/screen/menu/tab/Dessert/tab_dessert.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mycatering/screen/details/Detail.dart';
+import 'package:mycatering/screen/home/models/HomeModel.dart';
 import 'package:mycatering/screen/menu/tab/Dessert/title_dessert.dart';
 
 class DessertTab extends StatelessWidget {
@@ -30,10 +32,27 @@ class DessertTab extends StatelessWidget {
           mainAxisSpacing: 20,
         ),
         itemBuilder: (context, index) {
-          return DessertTitle(
-            DessertVariant: DessertOnSale[index][0],
-            DessertPrice: DessertOnSale[index][1],
-            imageDessertName: DessertOnSale[index][2],
+          FoodModel foodModel = dessertList[index];
+          return GestureDetector(
+            onTap: () {
+                    Navigator.of(context).push(
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) {
+                          return FadeTransition(
+                            opacity: animation,
+                            child: MyDetailPage(
+                              foodModel: foodModel,
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+            child: DessertTitle(
+              DessertVariant: DessertOnSale[index][0],
+              DessertPrice: DessertOnSale[index][1],
+              imageDessertName: DessertOnSale[index][2],
+            ),
           );
         },
       ),
